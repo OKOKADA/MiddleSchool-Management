dbaeceb9956035cbf1a93cf1f16f69378291a524
Falleiz <153763392+Falleiz@users.noreply.github.com>
2024-11-04 00:37:34 +0100

    fianlisation de l'installation du systeme

--- College_management/College_management/settings.py
+++ College_management/College_management/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app'
 ]
 
 MIDDLEWARE = [
@@ -115,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
 TIME_ZONE = 'UTC'
 
