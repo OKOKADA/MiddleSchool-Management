fd015067a25d60f6fe0f853e52f2ff7e32be2a5d
Amoajunior <junioradopo2002@gmail.com>
2025-01-03 16:53:47 +0100

    v 3

--- CollegeManagement/CollegeManagementapp/urls.py
+++ CollegeManagement/CollegeManagementapp/urls.py
@@ -4,6 +4,11 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
+    path('contact', views.contact, name="contact"),
+    path('login', views.loginUser, name="login"),
+    path('logout_user', views.logout_user, name="logout_user"),
+    path('registration', views.registration, name="registration"),
 
 ]
 
